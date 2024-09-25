b40c66da2f8e4edf2ed7200393e9a178afe794ab
Aditya Chincholkar <119576533+Aditya-132@users.noreply.github.com>
2024-09-25 18:03:52 +0530

    Update PreviousEducationDetails.jsx

--- src/components/Form_Components/PreviousEducationDetails.jsx
+++ src/components/Form_Components/PreviousEducationDetails.jsx
@@ -4,7 +4,7 @@ export const PreviousEducationDetails = ({ formData, handleChange, handleFileCha
     <div className="space-y-4">
         <InputField label="No of Backlogs" type="number" name="backlogs" value={formData.backlogs} onChange={handleChange} disabled={loading} required />
         <InputField label="Gap Year" type="number" name="gap_year" value={formData.gap_year} onChange={handleChange} disabled={loading} required />
-        <FileInput label="Upload Gap Year Proof" name="gap_yearProof" onChange={handleFileChange} disabled={loading} required />
+        <FileInput label="Upload Gap Year Proof" name="gap_yearProof" onChange={handleFileChange} disabled={loading}  />
         <InputField label="Projects" name="projects" value={formData.projects} onChange={handleChange} disabled={loading} required />
     </div>
 );
